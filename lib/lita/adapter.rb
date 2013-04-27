66081594bcbae17bef4a63ebb0bc7ffaac4f9574
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-04-27 12:43:35 -0700

    Remove old adapter registration mechanism.

--- lib/lita/adapter.rb
+++ lib/lita/adapter.rb
@@ -2,11 +2,6 @@ require "lita/util"
 
 module Lita
   class Adapter
-    def self.inherited(klass)
-      class_key = Util.underscore(Util.demodulize(klass.name)).to_sym
-      Lita.adapters[class_key] = klass
-    end
-
     def self.load_adapter(key)
       Lita.adapters[key.to_sym]
     end
--- spec/lita/adapter_spec.rb
+++ spec/lita/adapter_spec.rb
@@ -2,27 +2,18 @@ require "spec_helper"
 
 describe Lita::Adapter do
   describe ".load_adapter" do
+    let(:adapter_class) { double("adapter class") }
 
-    let!(:adapter_klass) do
-      module Lita
-        class TestAdapter < Adapter
-        end
-      end
+    before { Lita.register_adapter(:test_adapter, adapter_class) }
 
-      Lita::TestAdapter
-    end
-
-    after do
-      Lita.adapters.delete(:test_adapter)
-      Lita.send(:remove_const, :TestAdapter)
-    end
+    after { Lita.reset_registry }
 
     it "returns an adapter class from the registry for the given symbol" do
-      expect(described_class.load_adapter(:test_adapter)).to eq(adapter_klass)
+      expect(described_class.load_adapter(:test_adapter)).to eq(adapter_class)
     end
 
     it "allows the adapter key to be a string" do
-      expect(described_class.load_adapter("test_adapter")).to eq(adapter_klass)
+      expect(described_class.load_adapter("test_adapter")).to eq(adapter_class)
     end
   end
 end
