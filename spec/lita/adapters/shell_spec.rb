20b8930ef997844b93632a2a3ecf2542f1caf535
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-04-28 02:54:28 -0700

    Remove flaky spec for shell adapter.

--- spec/lita/adapters/shell_spec.rb
+++ spec/lita/adapters/shell_spec.rb
@@ -1,19 +1,4 @@
 require "spec_helper"
 
 describe Lita::Adapters::Shell do
-  subject { described_class.new(robot, config, stdout: stdout, stdin: stdin) }
-
-  let(:robot) { double("robot") }
-  let(:config) { double("config") }
-  let(:stdout) { StringIO.new }
-  let(:stdin) { StringIO.new }
-  let!(:thread) { Thread.new { subject.run } }
-
-  after { thread.kill if thread.alive? }
-
-  describe "#run" do
-    it "displays a message about how to exit on start up" do
-      expect(stdout.string).to match(/exit/)
-    end
-  end
 end
