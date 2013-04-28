b773962c3897446907b19507ee63139a5991a8be
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-04-28 13:59:53 -0700

    Add test coverage for shell adapter.

--- spec/lita/adapters/shell_spec.rb
+++ spec/lita/adapters/shell_spec.rb
@@ -1,4 +1,20 @@
 require "spec_helper"
 
 describe Lita::Adapters::Shell do
+  subject do
+    described_class.new(robot, config, stdout: stdout, stdin: stdin)
+  end
+
+  let(:robot) { double("robot") }
+  let(:config) { double("config") }
+  let(:stdout) { double("stdout") }
+  let(:stdin) { double("stdin") }
+
+  describe "#say" do
+    it "writes messages to stdout" do
+      stdout.should_receive(:puts).with("foo")
+      stdout.should_receive(:puts).with("bar")
+      subject.say("foo", "bar")
+    end
+  end
 end
