81a766b5c75355089dc4ecc10ef7487bce24a765
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-04-28 13:30:12 -0700

    Add test coverage for Robot.

--- spec/lita/robot_spec.rb
+++ spec/lita/robot_spec.rb
@@ -15,6 +15,8 @@ describe Lita::Robot do
 
   let(:storage) { double("storage") }
 
+  let(:message) { double("message") }
+
   before do
     Lita::Adapter.stub(:load_adapter) { adapter_class }
     Lita::Storage.stub(:new) { storage }
@@ -41,5 +43,26 @@ describe Lita::Robot do
 
   it "has a storage object" do
     expect(subject.storage).to be(storage)
+  end
+
+  describe "#receive" do
+    let(:handler_class) { double("handler class") }
+
+    after { Lita.reset_registry }
+
+    it "dispatches messages to all registered handlers" do
+      Lita.register_handler(handler_class)
+      handler_class.should_receive(:dispatch).with(subject, message)
+      subject.receive(message)
+    end
+  end
+
+  describe "#storage_for_handler" do
+    it "gets a Redis::Namespace object for a handler" do
+      storage.should_receive(:namespaced_storage).with(
+        "lita:handlers:my_handler"
+      )
+      subject.storage_for_handler(:my_handler)
+    end
   end
 end
