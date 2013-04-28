9b3a80dfea5cf5fa49661eed0a740d9ba3562671
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-04-28 03:01:10 -0700

    Add test coverage for Handler.

--- spec/lita/handler_spec.rb
+++ spec/lita/handler_spec.rb
@@ -2,52 +2,30 @@ require "spec_helper"
 
 describe Lita::Handler do
   let(:handler_class) do
-    Class.new(described_class) do
-      match /foo/
+    Class.new(described_class) { def foo; end }
+  end
+
+  describe ".listener" do
+    it "adds a listener to the registry" do
+      handler_class.listener(:foo, /foo/)
+      expect(handler_class.listeners).to eq([{ method: :foo, pattern: /foo/ }])
     end
   end
 
-  let(:message) do
-    message = double("message")
-    message.stub(:body) { "foo" }
-    message
-  end
+  describe ".dispatch" do
+    let(:robot) { double("robot") }
+    let(:message) { double("message") }
+    let(:handler) { double("handler") }
 
-  let(:unmatching_message) do
-    message = double("message")
-    message.stub(:body) { "bar" }
-    message
-  end
-
-  describe ".match" do
-    it "stores the string or pattern the message must match" do
-      expect(handler_class.match).to eq(/foo/)
-    end
-  end
-
-  describe ".match?" do
-    it "returns true if the message matches the pattern" do
-      expect(handler_class.match?(message)).to be_true
+    before do
+      handler_class.listener(:foo, /foo/)
+      message.stub(:body) { "foo" }
     end
 
-    it "returns false if the message doesn't match the pattern" do
-      expect(handler_class.match?(unmatching_message)).to be_false
-    end
-  end
-
-  describe ".description" do
-    it "sets and gets a description string" do
-      expect(handler_class.description).to be_false
-      handler_class.description "a description"
-      expect(handler_class.description).to eq("a description")
-    end
-  end
-
-  describe ".storage_key" do
-    it "sets and gets the key used to namespace the handler's storage" do
-      expect(handler_class.storage_key).to be_false
-      handler_class.storage_key :isolated_storage
-      expect(handler_class.storage_key).to eq(:isolated_storage)
+    it "calls every matching listener for a given message" do
+      handler_class.should_receive(:new) { handler }
+      handler.should_receive(:foo)
+      handler_class.dispatch(robot, message)
     end
   end
 end
