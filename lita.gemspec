74ad0544f5ba3a144e8c08984cc638ba4c0a5c34
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-06-08 04:53:00 -0700

    Add Travis, Code Climate, and Coveralls integration.

--- lita.gemspec
+++ lita.gemspec
@@ -23,4 +23,6 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "rake"
   spec.add_development_dependency "rspec", ">= 2.14.0rc1"
+  spec.add_development_dependency "simplecov"
+  spec.add_development_dependency "coveralls"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
+require "simplecov"
+require "coveralls"
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start
+
 require "lita"
 require "lita/rspec"
