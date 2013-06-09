80f70a21ea7ee17b402b4b583ccba26ad76d433f
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-06-09 00:43:15 -0700

    Add coverage for Shell adapter's run method.

--- spec/lita/adapters/shell_spec.rb
+++ spec/lita/adapters/shell_spec.rb
@@ -1,7 +1,20 @@
 describe Lita::Adapters::Shell do
-  let(:robot) { double("Robot") }
+  let(:robot) do
+    robot = double("Robot")
+    allow(robot).to receive(:name).and_return("Lita")
+    robot
+  end
 
   subject { described_class.new(robot) }
+
+  describe "#run" do
+    it "passes input to the Robot and breaks on an exit message" do
+      expect(subject).to receive(:print).with("#{robot.name} > ").twice
+      allow(subject).to receive(:gets).and_return("foo", "exit")
+      expect(robot).to receive(:receive).with("foo")
+      subject.run
+    end
+  end
 
   describe "#say" do
     it "prints its input" do
