3b75ba51d7dcb13469d8f001a62d3b0fdc4e4a46
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-06-12 00:41:15 -0700

    Add Adapter#require_config.

--- lib/lita/adapter.rb
+++ lib/lita/adapter.rb
@@ -2,8 +2,39 @@ module Lita
   class Adapter
     attr_reader :robot
 
+    class << self
+      attr_reader :required_configs
+
+      def require_config(*keys)
+        @required_configs ||= []
+        @required_configs.concat(keys.flatten.map(&:to_sym))
+      end
+
+      alias_method :require_configs, :require_config
+    end
+
     def initialize(robot)
       @robot = robot
+      ensure_required_configs
+    end
+
+    private
+
+    def ensure_required_configs
+      required_configs = self.class.required_configs
+      return if required_configs.nil?
+
+      missing_keys = []
+
+      required_configs.each do |key|
+        missing_keys << key unless Lita.config.adapter[key]
+      end
+
+      unless missing_keys.empty?
+        raise Lita::ConfigError.new(
+"The following keys are required on config.adapter: #{missing_keys.join(", ")}"
+        )
+      end
     end
   end
 end
--- spec/lita/adapter_spec.rb
+++ spec/lita/adapter_spec.rb
@@ -8,4 +8,22 @@ describe Lita::Adapter do
   it "stores a Robot" do
     expect(subject.robot).to eql(robot)
   end
+
+  describe ".require_config" do
+    let(:adapter_class) do
+      Class.new(described_class) do
+        require_config :foo
+        require_configs :bar, :baz
+        require_configs ["blah", :bleh]
+      end
+    end
+
+    subject { adapter_class.new(robot) }
+
+    it "ensures that config keys are present on initialization" do
+      expect do
+        subject
+      end.to raise_error(Lita::ConfigError, /foo, bar, baz, blah, bleh/)
+    end
+  end
 end
