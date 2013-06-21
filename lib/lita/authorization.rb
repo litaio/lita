2251351b4ca44c5f859cd240c1d95509f0dd9b5a
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-06-21 01:09:23 -0700

    Rename #admin? to #user_is_admin? and make it public.

--- lib/lita/authorization.rb
+++ lib/lita/authorization.rb
@@ -2,12 +2,12 @@ module Lita
   module Authorization
     class << self
       def add_user_to_group(user, group)
-        return unless admin?(user)
+        return unless user_is_admin?(user)
         redis.sadd(group, user.id)
       end
 
       def remove_user_from_group(user, group)
-        return unless admin?(user)
+        return unless user_is_admin?(user)
         redis.srem(group, user.id)
       end
 
@@ -15,11 +15,11 @@ module Lita
         redis.sismember(group, user.id)
       end
 
-      private
-
-      def admin?(user)
+      def user_is_admin?(user)
         Array(Lita.config.robot.admins).include?(user.id)
       end
+
+      private
 
       def redis
         @redis ||= Redis::Namespace.new("auth", redis: Lita.redis)
--- spec/lita/authorization_spec.rb
+++ spec/lita/authorization_spec.rb
@@ -8,32 +8,31 @@ describe Lita::Authorization, lita: true do
   end
 
   describe ".add_user_to_group" do
-    before { Lita.config.robot.admins = "1" }
-
     it "adds users to an auth group" do
+      allow(described_class).to receive(:user_is_admin?).and_return(true)
       described_class.add_user_to_group(user, "employees")
       expect(described_class.user_in_group?(user, "employees")).to be_true
     end
 
     it "can only be called by admins" do
-      Lita.config.robot.admins = nil
+      allow(described_class).to receive(:user_is_admin?).and_return(false)
       described_class.add_user_to_group(user, "employees")
       expect(described_class.user_in_group?(user, "employees")).to be_false
     end
   end
 
   describe ".remove_user_from_group" do
-    before { Lita.config.robot.admins = "1" }
-
     it "removes users from an auth group" do
+      allow(described_class).to receive(:user_is_admin?).and_return(true)
       described_class.add_user_to_group(user, "employees")
       described_class.remove_user_from_group(user, "employees")
       expect(described_class.user_in_group?(user, "employees")).to be_false
     end
 
     it "can only be called by admins" do
+      allow(described_class).to receive(:user_is_admin?).and_return(true)
       described_class.add_user_to_group(user, "employees")
-      Lita.config.robot.admins = nil
+      allow(described_class).to receive(:user_is_admin?).and_return(false)
       described_class.remove_user_from_group(user, "employees")
       expect(described_class.user_in_group?(user, "employees")).to be_true
     end
@@ -44,6 +43,18 @@ describe Lita::Authorization, lita: true do
 
     it "returns false if the user is in the group" do
       expect(described_class.user_in_group?(user, "employees")).to be_false
+    end
+  end
+
+  describe ".user_is_admin?" do
+    it "returns true if the user's ID is in the config" do
+      Lita.config.robot.admins = "1"
+      expect(described_class.user_is_admin?(user)).to be_true
+    end
+
+    it "returns false if the user's ID is not in the config" do
+      Lita.config.robot.admins = nil
+      expect(described_class.user_is_admin?(user)).to be_false
     end
   end
 end
