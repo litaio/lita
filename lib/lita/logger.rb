c8fe6baa996aad7d79c1dd4b9c6365c456e13000
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-07-05 22:22:37 -0700

    Document Lita::Logger.

--- lib/lita/logger.rb
+++ lib/lita/logger.rb
@@ -1,6 +1,11 @@
 module Lita
+  # Creates a Logger with the proper configuration.
   module Logger
     class << self
+      # Creates a new {::Logger} outputting to standard error with the given
+      # severity level and a custom format.
+      # @param level [Symbol, String] The name of the log level to use.
+      # @return [::Logger] The {::Logger} object.
       def get_logger(level)
         logger = ::Logger.new(STDERR)
         logger.level = get_level_constant(level)
@@ -12,6 +17,7 @@ module Lita
 
       private
 
+      # Gets the Logger constant for the given severity level.
       def get_level_constant(level)
         if level
           begin
