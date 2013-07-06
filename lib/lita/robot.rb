214d17cc007c0a220ece7141c4f6f601f7b05d56
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-07-05 23:12:50 -0700

    Document Lita::Robot.

--- lib/lita/robot.rb
+++ lib/lita/robot.rb
@@ -1,7 +1,21 @@
 module Lita
+  # The main object representing a running instance of Lita. Provides a high
+  # level API for the underlying adapter. Dispatches incoming messages to
+  # registered handlers. Can send outgoing chat messages and set the topic
+  # of chat rooms.
   class Robot
-    attr_reader :name, :app
+    # A +Rack+ application used for the built-in web server.
+    # @return [Rack::Builder] The +Rack+ app.
+    attr_reader :app
+
+    # The name the robot will look for in incoming messages to determine if it's
+    # being addressed.
+    # @return [String] The mention name.
     attr_accessor :mention_name
+
+    # The name of the robot as it will appear in the chat.
+    # @return [String] The robot's name.
+    attr_reader :name
 
     def initialize
       @name = Lita.config.robot.name
@@ -10,10 +24,17 @@ module Lita
       load_adapter
     end
 
+    # The primary entry point from the adapter for an incoming message.
+    # Dispatches the message to all registered handlers.
+    # @param message [Lita::Message] The incoming message.
+    # @return [void]
     def receive(message)
       Lita.handlers.each { |handler| handler.dispatch(self, message) }
     end
 
+    # Starts the robot, booting the web server and delegating to the adapter to
+    # connect to the chat service.
+    # @return [void]
     def run
       run_app
       @adapter.run
@@ -21,15 +42,29 @@ module Lita
       shut_down
     end
 
+    # Sends one or more messages to a user or room.
+    # @param target [Lita::Source] The user or room to send to. If the Source
+    #   has a room, it will choose the room. Otherwise, it will send to the
+    #   user.
+    # @param strings [String, Array<String>] One or more strings to send.
+    # @return [void]
     def send_messages(target, *strings)
       @adapter.send_messages(target, strings.flatten)
     end
     alias_method :send_message, :send_messages
 
+    # Sets the topic for a chat room.
+    # @param target [Lita::Source] A source object specifying the room.
+    # @param topic [String] The new topic message to set.
+    # @return [void]
     def set_topic(target, topic)
       @adapter.set_topic(target, topic)
     end
 
+    # Gracefully shuts the robot down, stopping the web server and delegating
+    # to the adapter to perform any shut down tasks necessary for the chat
+    # service.
+    # @return [void]
     def shut_down
       @server.stop if @server
       @server_thread.join if @server_thread
@@ -38,6 +73,7 @@ module Lita
 
     private
 
+    # Loads the selected adapter.
     def load_adapter
       adapter_name = Lita.config.robot.adapter
       adapter_class = Lita.adapters[adapter_name.to_sym]
@@ -50,6 +86,7 @@ module Lita
       @adapter = adapter_class.new(self)
     end
 
+    # Starts the web server.
     def run_app
       @server_thread = Thread.new do
         @server = Thin::Server.new(
