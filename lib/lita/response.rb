06ec1681452defc51ad929f3a845905a03fa67f9
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-07-05 22:52:31 -0700

    Document Lita::Response.

--- lib/lita/response.rb
+++ lib/lita/response.rb
@@ -1,11 +1,27 @@
 module Lita
+  # A wrapper object that provides the primary interface for handlers to
+  # respond to incoming chat messages.
   class Response
     extend Forwardable
 
-    attr_accessor :message, :matches
+    # The incoming message.
+    # @return [Lita::Message] The message.
+    attr_accessor :message
 
+    # An array of matches from running the message against the route pattern.
+    # @return [Array<String>, Array<Array<String>>] The array of matches.
+    attr_accessor :matches
+
+    # @!method args
+    #   @see Lita::Message#args
+    # @!method reply
+    #   @see Lita::Message#reply
+    # @!method user
+    #   @see Lita::Message#user
     def_delegators :message, :args, :reply, :user
 
+    # @param message [Lita::Message] The incoming message.
+    # @param matches [Array<String>, Array<Array<String>>] The Regexp matches.
     def initialize(message, matches: nil)
       self.message = message
       self.matches = matches
