41c3b6e3fe9f573712c93a5bda6f6b7f41649763
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-08-06 03:53:38 -0700

    Split Lita::RSpec.included into multiple methods.

--- lib/lita/rspec.rb
+++ lib/lita/rspec.rb
@@ -16,20 +16,36 @@ require "lita/rspec/handler"
 module Lita
   # Extras for +RSpec+ that facilitate the testing of Lita code.
   module RSpec
-    # Causes all interaction with Redis to use a test-specific namespace. Clears
-    # Redis before each example. Stubs the logger to prevent log messages from
-    # cluttering test output. Clears Lita's global configuration.
-    # @param base [Object] The class including the module.
-    # @return [void]
-    def self.included(base)
-      base.class_eval do
-        before do
-          stub_const("Lita::REDIS_NAMESPACE", "lita.test")
-          keys = Lita.redis.keys("*")
-          Lita.redis.del(keys) unless keys.empty?
-          logger = double("Logger").as_null_object
-          allow(Lita).to receive(:logger).and_return(logger)
-          Lita.clear_config
+    class << self
+      # Causes all interaction with Redis to use a test-specific namespace.
+      # Clears Redis before each example. Stubs the logger to prevent log
+      # messages from cluttering test output. Clears Lita's global
+      # configuration.
+      # @param base [Object] The class including the module.
+      # @return [void]
+      def included(base)
+        base.class_eval do
+          before do
+            logger = double("Logger").as_null_object
+            allow(Lita).to receive(:logger).and_return(logger)
+            Lita.clear_config
+          end
+        end
+
+        set_up_redis(base)
+      end
+
+      private
+
+      # Set up Redis to use the test namespace and clear out before each
+      # example.
+      def set_up_redis(base)
+        base.class_eval do
+          before do
+            stub_const("Lita::REDIS_NAMESPACE", "lita.test")
+            keys = Lita.redis.keys("*")
+            Lita.redis.del(keys) unless keys.empty?
+          end
         end
       end
     end
