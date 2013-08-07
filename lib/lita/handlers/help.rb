ec332cb5da7c77c42e742b321fdf9431896c6afd
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-08-06 20:43:26 -0700

    Don't show help for commands the user cannot access.

--- lib/lita/handlers/help.rb
+++ lib/lita/handlers/help.rb
@@ -15,26 +15,47 @@ Lists help information for terms or commands that begin with COMMAND.
       # @param response [Lita::Response] The response object.
       # @return [void]
       def help(response)
+        output = build_help(response)
+        output = filter_help(output, response)
+        response.reply output.join("\n")
+      end
+
+      private
+
+      # Checks if the user is authorized to at least one of the given groups.
+      def authorized?(user, required_groups)
+        required_groups.nil? || required_groups.any? do |group|
+          Lita::Authorization.user_in_group?(user, group)
+        end
+      end
+
+      # Creates an array of help info for all registered routes.
+      def build_help(response)
         output = []
 
         Lita.handlers.each do |handler|
           handler.routes.each do |route|
             route.help.each do |command, description|
+              next unless authorized?(response.user, route.required_groups)
               command = "#{name}: #{command}" if route.command?
               output << "#{command} - #{description}"
             end
           end
         end
 
-        filter = response.matches[0][0]
-        if filter
-          output.select! { |line| /(?:@?#{name}[:,]?)?#{filter}/i === line }
-        end
-
-        response.reply output.join("\n")
+        output
       end
 
-      private
+      # Filters the help output by an optional command.
+      def filter_help(output, response)
+        filter = response.matches[0][0]
+
+        if filter
+          output.select { |line| /(?:@?#{name}[:,]?)?#{filter}/i === line }
+        else
+          output
+        end
+      end
 
       # The way the bot should be addressed in order to trigger a command.
       def name
