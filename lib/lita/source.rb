de4d35946bfa12bd6c2b7f9525b227aeb9db7154
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-09-17 11:11:40 -0700

    Add new API for Source objects that allows explicitly marking them as private. Fixes #18.

--- lib/lita/source.rb
+++ lib/lita/source.rb
@@ -1,11 +1,16 @@
 module Lita
-  # A wrapper object representing the source of an incoming message (the user
-  # who sent it, and optionally the room they sent it from). If a room is set,
+  # A wrapper object representing the source of an incoming message (either the
+  # user who sent it, the room they sent it from, or both). If a room is set,
   # the message is from a group chat room. If no room is set, the message is
-  # assumed to be a private message. Source objects are also used as "target"
-  # objects when sending an outgoing message or performing another operation
-  # on a user or a room.
+  # assumed to be a private message, though Source objects can be explicitly
+  # marked as private messages. Source objects are also used as "target" objects
+  # when sending an outgoing message or performing another operation on a user
+  # or a room.
   class Source
+    # A flag indicating that a message was sent to the robot privately.
+    # @return [Boolean] The boolean flag.
+    attr_reader :private_message
+    alias_method :private_message?, :private_message
 
     # The room the message came from or should be sent to.
     # @return [String] A string uniquely identifying the room.
@@ -20,9 +25,29 @@ module Lita
     # @param room [String] A string uniquely identifying the room the user sent
     #   the message from, or the room where a reply should go. The format of
     #   this string will differ depending on the chat service.
-    def initialize(user, room = nil)
-      @user = user
-      @room = room
+    # @param private_message [Boolean] A flag indicating whether or not the
+    #   message was sent privately.
+    def initialize(*args)
+      options = args.last.is_a?(Hash) ? args.pop : {}
+
+      @user = options[:user]
+      @room = options[:room]
+      @private_message = options[:private_message]
+
+      if args.size > 0
+        Lita.logger.warn <<-WARNING.chomp
+Passing positional arguments to Source is deprecated. \
+Use Source.new(user: user, room: room, private_message: pm) instead.
+WARNING
+        @user = args[0] if args[0]
+        @room = args[1] if args[1]
+      end
+
+      if user.nil? && room.nil?
+        raise ArgumentError.new("Either a user or a room is required.")
+      end
+
+      @private_message = true if room.nil?
     end
   end
 end
--- spec/lita/source_spec.rb
+++ spec/lita/source_spec.rb
@@ -2,12 +2,37 @@ require "spec_helper"
 
 describe Lita::Source do
   it "has a user" do
-    subject = described_class.new("Carl")
+    subject = described_class.new(user: "Carl")
     expect(subject.user).to eq("Carl")
   end
 
   it "has a room" do
-    subject = described_class.new("Carl", "#litabot")
+    subject = described_class.new(room: "#litabot")
     expect(subject.room).to eq("#litabot")
+  end
+
+  it "has a private message flag" do
+    subject = described_class.new(user: "Carl", private_message: true)
+    expect(subject.private_message).to be_true
+  end
+
+  it "requires either a user or a room" do
+    expect { described_class.new }.to raise_error(ArgumentError)
+  end
+
+  describe "the deprecated Source.new(user, room) API" do
+    it "can have a user and is marked as private if there is no room" do
+      expect(Lita.logger).to receive(:warn)
+      subject = described_class.new("Carl")
+      expect(subject.user).to eq("Carl")
+      expect(subject).to be_a_private_message
+    end
+
+    it "can have a room and is not marked as private if it does" do
+      expect(Lita.logger).to receive(:warn)
+      subject = described_class.new("Carl", "#litabot")
+      expect(subject.room).to eq("#litabot")
+      expect(subject).not_to be_a_private_message
+    end
   end
 end
