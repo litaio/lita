a7cc4d1ef33ebf6dc561e3d79134f00765af2654
Jimmy Cuadra <jimmy@jimmycuadra.com>
2014-01-26 14:49:51 -0800

    Use accessor methods instead of instance variables.

--- lib/lita/rspec/matchers/route_matcher.rb
+++ lib/lita/rspec/matchers/route_matcher.rb
@@ -3,13 +3,14 @@ module Lita
     module Matchers
       # Used to complete a chat routing test chain.
       class RouteMatcher
-        attr_accessor :expected_route
+        attr_accessor :context, :inverted, :message_body
+        attr_reader :expected_route
+        alias_method :inverted?, :inverted
 
         def initialize(context, message_body, invert: false)
-          @context = context
-          @message_body = message_body
-          @method = invert ? :not_to : :to
-          @inverted = invert
+          self.context = context
+          self.message_body = message_body
+          self.inverted = invert
           set_description
         end
 
@@ -21,10 +22,10 @@ module Lita
         def to(route)
           self.expected_route = route
 
-          m = @method
-          b = @message_body
+          m = method
+          b = message_body
 
-          @context.instance_eval do
+          context.instance_eval do
             allow(Authorization).to receive(:user_in_group?).and_return(true)
             expect_any_instance_of(described_class).public_send(m, receive(route))
             send_message(b)
@@ -46,12 +47,16 @@ module Lita
           set_description
         end
 
-        def inverted?
-          defined?(@inverted) && @inverted
+        def method
+          if inverted?
+            :not_to
+          else
+            :to
+          end
         end
 
         def set_description
-          description = %{#{description_prefix} "#{@message_body}"}
+          description = %{#{description_prefix} "#{message_body}"}
           description << " to :#{expected_route}" if expected_route
           ::RSpec.current_example.metadata[:description] = description
         end
