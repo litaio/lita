3bc6260f9cbd5feb242b724860baf269ea5e7d82
Jimmy Cuadra <jimmy@jimmycuadra.com>
2014-01-26 14:55:32 -0800

    Set example description for HTTP routes.

--- lib/lita/rspec/matchers/http_route_matcher.rb
+++ lib/lita/rspec/matchers/http_route_matcher.rb
@@ -3,11 +3,16 @@ module Lita
     module Matchers
       # Used to complete an HTTP routing test chain.
       class HTTPRouteMatcher
+        attr_accessor :context, :http_method, :inverted, :path
+        attr_reader :expected_route
+        alias_method :inverted?, :inverted
+
         def initialize(context, http_method, path, invert: false)
-          @context = context
-          @http_method = http_method
-          @path = path
-          @method = invert ? :not_to : :to
+          self.context = context
+          self.http_method = http_method
+          self.path = path
+          self.inverted = invert
+          set_description
         end
 
         # Sets an expectation that an HTTP route will or will not be triggered,
@@ -17,15 +22,46 @@ module Lita
         #   be triggered.
         # @return [void]
         def to(route)
-          m = @method
-          h = @http_method
-          p = @path
+          self.expected_route = route
 
-          @context.instance_eval do
+          m = method
+          h = http_method
+          p = path
+
+          context.instance_eval do
             expect_any_instance_of(described_class).public_send(m, receive(route))
             env = Rack::MockRequest.env_for(p, method: h)
             robot.app.call(env)
           end
+        end
+
+        private
+
+        def description_prefix
+          if inverted?
+            "doesn't route"
+          else
+            "routes"
+          end
+        end
+
+        def expected_route=(route)
+          @expected_route = route
+          set_description
+        end
+
+        def method
+          if inverted?
+            :not_to
+          else
+            :to
+          end
+        end
+
+        def set_description
+          description = "#{description_prefix} #{http_method.upcase} #{path}"
+          description << " to :#{expected_route}" if expected_route
+          ::RSpec.current_example.metadata[:description] = description
         end
       end
     end
