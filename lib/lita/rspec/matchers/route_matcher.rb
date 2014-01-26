58be35cc15399a8adac52a66c0a8ff2e1e3ce942
Jimmy Cuadra <jimmy@jimmycuadra.com>
2014-01-26 14:30:51 -0800

    Set example description for chat routes.

--- lib/lita/rspec/matchers/route_matcher.rb
+++ lib/lita/rspec/matchers/route_matcher.rb
@@ -3,10 +3,14 @@ module Lita
     module Matchers
       # Used to complete a chat routing test chain.
       class RouteMatcher
+        attr_accessor :expected_route
+
         def initialize(context, message_body, invert: false)
           @context = context
           @message_body = message_body
           @method = invert ? :not_to : :to
+          @inverted = invert
+          set_description
         end
 
         # Sets an expectation that a route will or will not be triggered, then
@@ -15,6 +19,8 @@ module Lita
         #   be triggered.
         # @return [void]
         def to(route)
+          self.expected_route = route
+
           m = @method
           b = @message_body
 
@@ -23,6 +29,31 @@ module Lita
             expect_any_instance_of(described_class).public_send(m, receive(route))
             send_message(b)
           end
+        end
+
+        private
+
+        def description_prefix
+          if inverted?
+            "doesn't route"
+          else
+            "routes"
+          end
+        end
+
+        def expected_route=(route)
+          @expected_route = route
+          set_description
+        end
+
+        def inverted?
+          defined?(@inverted) && @inverted
+        end
+
+        def set_description
+          description = %{#{description_prefix} "#{@message_body}"}
+          description << " to :#{expected_route}" if expected_route
+          ::RSpec.current_example.metadata[:description] = description
         end
       end
     end
