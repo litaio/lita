1b8cd6e0b27b080dfdda21de169bfb7c5c364a2f
Jimmy Cuadra <jimmy@jimmycuadra.com>
2014-02-19 01:46:54 -0800

    Add Room handler to manage joining and parting. Refs #39.

---
+++ lib/lita/handlers/room.rb
@@ -0,0 +1,30 @@
+module Lita
+  module Handlers
+    # Allows administrators to make Lita join and part from rooms.
+    class Room < Handler
+      route(/^join\s+(.+)$/i, :join, command: true, restrict_to: :admins, help: {
+        t("help.join_key") => t("help.join_value")
+      })
+
+      route(/^part\s+(.+)$/i, :part, command: true, restrict_to: :admins, help: {
+        t("help.part_key") => t("help.part_value")
+      })
+
+      # Joins the room with the specified ID.
+      # @param response [Lita::Response] The response object.
+      # @return [void]
+      def join(response)
+        robot.join(response.args[0])
+      end
+
+      # Parts from the room with the specified ID.
+      # @param response [Lita::Response] The response object.
+      # @return [void]
+      def part(response)
+        robot.part(response.args[0])
+      end
+    end
+
+    Lita.register_handler(Room)
+  end
+end
---
+++ spec/lita/handlers/room_spec.rb
@@ -0,0 +1,20 @@
+describe Lita::Handlers::Room, lita_handler: true do
+  it { routes_command("join #lita.io").to(:join) }
+  it { routes_command("part #lita.io").to(:part) }
+
+  before { allow(Lita::Authorization).to receive(:user_is_admin?).with(user).and_return(true) }
+
+  describe "#join" do
+    it "calls Robot#join with the provided ID" do
+      expect(robot).to receive(:join).with("#lita.io")
+      send_command("join #lita.io")
+    end
+  end
+
+  describe "#part" do
+    it "calls Robot#part with the provided ID" do
+      expect(robot).to receive(:part).with("#lita.io")
+      send_command("part #lita.io")
+    end
+  end
+end
