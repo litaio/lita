153e760a04cd3b52c29ed1b1184fb3167198ee78
Jimmy Cuadra <jimmy@jimmycuadra.com>
2014-04-27 04:21:39 -0700

    Update comment for new issue blocking partial double verification. [ci skip]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,8 +14,8 @@ require "lita/rspec"
 RSpec.configure do |config|
   config.mock_with :rspec do |mocks_config|
     mocks_config.verify_doubled_constant_names = true
-    # Enable config option this when a new rspec-mocks beta including this patch is released:
-    # https://github.com/rspec/rspec-mocks/pull/466
+    # Enable config option when a new rspec-mocks beta including this patch is released:
+    # https://github.com/rspec/rspec-mocks/pull/615
     #
     # mocks_config.verify_partial_doubles = true
   end
