622f99d3c9d06acff8345ec447dc08e7d204171d
Jimmy Cuadra <jimmy@jimmycuadra.com>
2014-06-02 01:47:09 -0700

    Upgrade to RSpec 3.0.0.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,9 +14,6 @@ require "lita/rspec"
 RSpec.configure do |config|
   config.mock_with :rspec do |mocks_config|
     mocks_config.verify_doubled_constant_names = true
-    # Enable config option when a new rspec-mocks beta including this patch is released:
-    # https://github.com/rspec/rspec-mocks/pull/615
-    #
-    # mocks_config.verify_partial_doubles = true
+    mocks_config.verify_partial_doubles = true
   end
 end
