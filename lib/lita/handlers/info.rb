50c55bf1807108cabe65843a44607bd3859a8aed
Jimmy Cuadra <jimmy@jimmycuadra.com>
2014-06-12 13:28:52 -0700

    Add Redis version and memory usage to info handler responses.

--- lib/lita/handlers/info.rb
+++ lib/lita/handlers/info.rb
@@ -9,12 +9,16 @@ module Lita
 
       http.get "/lita/info", :web
 
-      # Replies with the current version of the Lita.
+      # Replies with the current version of Lita, the current version of Redis,
+      # and Redis memory usage.
       # @param response [Lita::Response] The response object.
       # @return [void]
       # @since 3.0.0
       def chat(response)
-        response.reply "Lita #{Lita::VERSION} - https://www.lita.io/"
+        response.reply(
+          %(Lita #{Lita::VERSION} - https://www.lita.io/),
+          %(Redis #{redis_version} - Memory used: #{redis_memory_usage})
+        )
       end
 
       # Returns JSON with basic information about the robot.
@@ -24,12 +28,29 @@ module Lita
       def web(_request, response)
         response.headers["Content-Type"] = "application/json"
         json = MultiJson.dump(
-          lita_version: Lita::VERSION,
           adapter: Lita.config.robot.adapter,
-          robot_name: robot.name,
-          robot_mention_name: robot.mention_name
+          lita_version: Lita::VERSION,
+          redis_memory_usage: redis_memory_usage,
+          redis_version: redis_version,
+          robot_mention_name: robot.mention_name,
+          robot_name: robot.name
         )
         response.write(json)
+      end
+
+      # A hash of information about Redis.
+      def redis_info
+        @redis_info ||= redis.info
+      end
+
+      # The current version of Redis.
+      def redis_version
+        redis_info["redis_version"]
+      end
+
+      # The amount of memory Redis is using.
+      def redis_memory_usage
+        redis_info["used_memory_human"]
       end
     end
 
--- spec/lita/handlers/info_spec.rb
+++ spec/lita/handlers/info_spec.rb
@@ -10,20 +10,56 @@ describe Lita::Handlers::Info, lita_handler: true do
   describe "#chat" do
     it "responds with the current version of Lita" do
       send_command("info")
-      expect(replies.last).to include(Lita::VERSION)
+      expect(replies.first).to include(Lita::VERSION)
     end
 
     it "responds with a link to the website" do
       send_command("info")
-      expect(replies.last).to include("lita.io")
+      expect(replies.first).to include("lita.io")
+    end
+
+    it "responds with the Redis version and memory usage" do
+      send_command("info")
+      expect(replies.last).to match(/Redis [\d\.]+ - Memory used: [\d\.]+[BKMG]/)
     end
   end
 
   describe "#web" do
-    it "returns JSON with info about the running robot" do
+    let(:json) { MultiJson.load(response.body.join) }
+
+    it "returns JSON" do
       subject.web(request, response)
       expect(response.headers["Content-Type"]).to eq("application/json")
-      expect(response.body.join).to include(%("lita_version":"#{Lita::VERSION}"))
+    end
+
+    it "includes the current version of Lita" do
+      subject.web(request, response)
+      expect(json).to include("lita_version" => Lita::VERSION)
+    end
+
+    it "includes the adapter being used" do
+      subject.web(request, response)
+      expect(json).to include("adapter" => Lita.config.robot.adapter.to_s)
+    end
+
+    it "includes the robot's name" do
+      subject.web(request, response)
+      expect(json).to include("robot_name" => robot.name)
+    end
+
+    it "includes the robot's mention name" do
+      subject.web(request, response)
+      expect(json).to include("robot_mention_name" => robot.mention_name)
+    end
+
+    it "includes the Redis version" do
+      subject.web(request, response)
+      expect(json).to have_key("redis_version")
+    end
+
+    it "includes the Redis memory usage" do
+      subject.web(request, response)
+      expect(json).to have_key("redis_memory_usage")
     end
   end
 end
