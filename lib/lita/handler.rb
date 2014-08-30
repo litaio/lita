9ea007d2b16d37bc2a61fd6f35a9334725d3f97b
Jimmy Cuadra <jimmy@jimmycuadra.com>
2014-08-30 08:00:09 -0700

    Add examples for Handler to ensure it includes each type of router.

--- lib/lita/handler.rb
+++ lib/lita/handler.rb
@@ -1,7 +1,6 @@
 module Lita
   # Base class for objects that add new behavior to Lita.
   class Handler
-    extend Forwardable
     extend ChatRouter
     extend HTTPRouter
     extend EventRouter
--- spec/lita/handler_spec.rb
+++ spec/lita/handler_spec.rb
@@ -1,4 +1,40 @@
 require "spec_helper"
 
-describe Lita::Handler, lita: true do
+describe Lita::Handler, lita_handler: true do
+  before { registry.handlers.delete(described_class) }
+
+  it "includes chat routes" do
+    registry.register_handler(:foo) do
+      route(/foo/) do |response|
+        response.reply("bar")
+      end
+    end
+
+    send_message("foo")
+
+    expect(replies.last).to include("bar")
+  end
+
+  it "includes HTTP routes" do
+    registry.register_handler(:foo) do
+      http.get "foo" do |_request, response|
+        response.write("bar")
+      end
+    end
+
+    http_client = Faraday::Connection.new { |c| c.adapter(:rack, Lita::RackApp.new(robot)) }
+    response = http_client.get("/foo")
+
+    expect(response.body).to eq("bar")
+  end
+
+  it "includes event routes" do
+    registry.register_handler(:foo) do
+      on(:some_event) { robot.send_message("payload received") }
+    end
+
+    expect(robot).to receive(:send_message).with("payload received")
+
+    robot.trigger(:some_event)
+  end
 end
