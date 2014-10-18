3593b0e064adecd6a26b1706bcb343f5733819ff
Jimmy Cuadra <jimmy@jimmycuadra.com>
2014-10-17 20:56:23 -0700

    Add API documentation to ConfigurationValidator.

--- lib/lita/configuration_validator.rb
+++ lib/lita/configuration_validator.rb
@@ -1,9 +1,14 @@
 module Lita
+  # Validates a registry's configuration, checking for required attributes that are missing.
   class ConfigurationValidator
+    # @param registry [Lita::Registry] The registry to validate.
     def initialize(registry)
       @registry = registry
     end
 
+    # Validates adapter and handler configuration.
+    # @return [void]
+    # @raise [Lita::ValidationError] If any required configuration attributes are missing.
     def call
       validate_adapters
       validate_handlers
@@ -11,30 +16,37 @@ module Lita
 
     private
 
+    # The registry's adapters.
     def adapters
       @registry.adapters
     end
 
+    # All a plugin's top-level configuration attributes.
     def children_for(plugin)
       plugin.configuration.children
     end
 
+    # Generates the fully qualified name of a configuration attribute.
     def full_attribute_name(names, name)
       (names + [name]).join(".")
     end
 
+    # The registry's handlers.
     def handlers
       @registry.handlers
     end
 
+    # Validates the registry's adapters.
     def validate_adapters
       adapters.each_value { |adapter| validate(:adapter, adapter, children_for(adapter)) }
     end
 
+    # Validates the registry's handlers.
     def validate_handlers
       handlers.each { |handler| validate(:handler, handler, children_for(handler)) }
     end
 
+    # Validates an array of attributes, recursing if any nested attributes are encountered.
     def validate(type, plugin, attributes, attribute_namespace = [])
       attributes.each do |attribute|
         if attribute.children?
