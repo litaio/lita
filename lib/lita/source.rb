f507bb0b02940b35b1d9ce3218714a0a5046c460
Jimmy Cuadra <jimmy@jimmycuadra.com>
2015-06-10 00:08:02 -0700

    Add Source#room_object.

--- lib/lita/source.rb
+++ lib/lita/source.rb
@@ -12,24 +12,38 @@ module Lita
     attr_reader :private_message
     alias_method :private_message?, :private_message
 
-    # The room the message came from or should be sent to.
-    # @return [String] A string uniquely identifying the room.
+    # The room the message came from or should be sent to, as a string.
+    # @return [String, NilClass] A string uniquely identifying the room.
     attr_reader :room
 
+    # The room the message came from or should be sent to, as a {Lita::Room} object.
+    # @return [Lita::Room, NilClass] The room.
+    # @since 4.4.0
+    attr_reader :room_object
+
     # The user who sent the message or should receive the outgoing message.
-    # @return [Lita::User] The user.
+    # @return [Lita::User, NilClass] The user.
     attr_reader :user
 
     # @param user [Lita::User] The user who sent the message or should receive
     #   the outgoing message.
-    # @param room [String] A string uniquely identifying the room the user sent
-    #   the message from, or the room where a reply should go. The format of
-    #   this string will differ depending on the chat service.
+    # @param room [Lita::Room, String] A string or {Lita::Room} uniquely identifying the room
+    #   the user sent the message from, or the room where a reply should go. The format of this
+    #   string (or the ID of the {Lita::Room} object) will differ depending on the chat service.
     # @param private_message [Boolean] A flag indicating whether or not the
     #   message was sent privately.
     def initialize(user: nil, room: nil, private_message: false)
       @user = user
-      @room = room
+
+      case room
+      when String
+        @room = room
+        @room_object = Room.new(room)
+      when Room
+        @room = room.id
+        @room_object = room
+      end
+
       @private_message = private_message
 
       raise ArgumentError, I18n.t("lita.source.user_or_room_required") if user.nil? && room.nil?
--- spec/lita/source_spec.rb
+++ spec/lita/source_spec.rb
@@ -1,25 +1,54 @@
 require "spec_helper"
 
 describe Lita::Source do
-  it "has a user" do
-    subject = described_class.new(user: "Carl")
-    expect(subject.user).to eq("Carl")
+  subject { described_class.new(user: user, room: room, private_message: pm) }
+
+  let(:pm) { false }
+  let(:room) { Lita::Room.new(1) }
+  let(:user) { Lita::User.new(1) }
+
+  describe "#room" do
+    it "returns the room as a string" do
+      expect(subject.room).to eq("1")
+    end
   end
 
-  it "has a room" do
-    subject = described_class.new(room: "#litabot")
-    expect(subject.room).to eq("#litabot")
+  describe "#room_object" do
+    it "returns the room as a Lita::Room" do
+      expect(subject.room_object).to eq(room)
+    end
   end
 
-  it "has a private message flag" do
-    subject = described_class.new(user: "Carl", private_message: true)
-    expect(subject).to be_a_private_message
+  describe "#user" do
+    it "returns the user object" do
+      expect(subject.user).to eq(user)
+    end
+  end
+
+  context "when the private_message argument is true" do
+    let(:pm) { true }
+
+    it "is marked as a private message" do
+      expect(subject).to be_a_private_message
+    end
   end
 
   it "can be manually marked as private" do
-    subject = described_class.new(user: "Carl", room: "#litabot")
     subject.private_message!
+
     expect(subject).to be_a_private_message
+  end
+
+  context "with a string for the room argument" do
+    let(:room) { "#channel" }
+
+    it "sets #room to the string" do
+      expect(subject.room).to eq(room)
+    end
+
+    it "sets #room_object to a Lita::Room with the string as its ID" do
+      expect(subject.room_object).to eq(Lita::Room.new(room))
+    end
   end
 
   it "requires either a user or a room" do
