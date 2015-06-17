f9498ee6e3497760e706f212f877072ab2eae0ee
Jeremy Nicklas <nickjer@hotmail.com>
2015-06-17 18:19:26 -0400

    fix the tests run on renamed Room class

--- spec/lita/handlers/room_spec.rb
+++ spec/lita/handlers/room_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-describe Lita::Handlers::Room, lita_handler: true do
+describe Lita::Handlers::Rooms, lita_handler: true do
   it { is_expected.to route_command("join #lita.io").to(:join) }
   it { is_expected.to route_command("part #lita.io").to(:part) }
 
