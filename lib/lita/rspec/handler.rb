b9443b6bcb7e22ece8f6995c6f63d179e95712c9
Jeremy Nicklas <nickjer@hotmail.com>
2015-06-17 19:00:48 -0400

    change to use room object

--- lib/lita/rspec/handler.rb
+++ lib/lita/rspec/handler.rb
@@ -78,7 +78,7 @@ module Lita
       # Sends a message to the robot.
       # @param body [String] The message to send.
       # @param as [Lita::User] The user sending the message.
-      # @param as [String] The room where the message is received from.
+      # @param as [Lita::Room] The room where the message is received from.
       # @return [void]
       def send_message(body, as: user, from: nil)
         message = Message.new(robot, body, Source.new(user: as, room: from))
